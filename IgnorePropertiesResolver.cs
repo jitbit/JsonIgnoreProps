3c2078fae0a439c75a109f93185650aa2bc041c3
Alexander Yumashev <33555768+alex-jitbit@users.noreply.github.com>
2019-12-07 17:07:57 +0200

    Create IgnorePropertiesResolver.cs

---
+++ IgnorePropertiesResolver.cs
@@ -0,0 +1,31 @@
+using Newtonsoft.Json.Serialization;
+using System.Reflection;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jitbit.Utils
+{
+	//helper to ignore some properties from serialization
+	public class IgnorePropertiesResolver : DefaultContractResolver
+	{
+		private IEnumerable<string> _propsToIgnore;
+
+		public IgnorePropertiesResolver(IEnumerable<string> propNamesToIgnore)
+		{
+			_propsToIgnore = propNamesToIgnore;
+		}
+
+		protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+		{
+			JsonProperty property = base.CreateProperty(member, memberSerialization);
+
+			if (_propsToIgnore.Contains(property.PropertyName))
+			{
+				property.ShouldSerialize = (x) => { return false; };
+			}
+
+			return property;
+		}
+	}
+}
